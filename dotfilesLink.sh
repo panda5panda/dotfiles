9b8a74ec51e1a1667336b665cc9430034db7b0d7
Takuya Matsuyama <hi@craftz.dog>
2020-12-09 23:14:09 +0900

    chore: renmae tmux script and adjust ln path

--- dotfilesLink.sh
+++ dotfilesLink.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
-ln -sf ~/dotfiles/.config/nvim ~/.config/nvim
+ln -sf ~/dotfiles/.config/nvim ~/.config/
 ln -sf ~/dotfiles/.tmux.conf ~/
 ln -sf ~/dotfiles/.vimrc.keymap ~/
+ln -sf ~/dotfiles/ide /usr/local/bin/
